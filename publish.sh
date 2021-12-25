afe008e83bee6886741364130fb9ca5ec754f920
thiswind <thiswind@gmail.com>
2021-12-25 09:12:46 +0000

    update

--- publish.sh
+++ publish.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-rm -rf _site &&\
-mkdir -p _site/ &&\
-git clone -b gh-pages `git config remote.origin.url` _site &&\
-git config pull.rebase false
+if [ "$(cat FREEZE_QRCODE)" == "True" ]; then
+    echo "QR code already published."
+    exit 0
+else
+    echo "Publishing QR code ..."
+    python create_cases.py
+    echo "QR code published."
+fi
+
+rm -rf _site &&
+    mkdir -p _site/ &&
+    git clone -b gh-pages "$(git config remote.origin.url)" _site &&
+    git config pull.rebase false
 
 jekyll build
 
-cd _site &&\
-git config pull.rebase false &&\
-rm -f README.md &&\
-git add -A &&\
-git commit -a -m "update" &&\
-git push ;\
+cd _site &&
+    git config pull.rebase false &&
+    rm -f README.md &&
+    git add -A &&
+    git commit -a -m "update" &&
+    git push
 cd ..
